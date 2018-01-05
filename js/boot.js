a9ea3d5263b5e58adf9f1c152f504b8a95e809e1
psycroStudio <zac.bragg@live.com>
2018-01-04 20:56:46 -0800

    daily changes, private scope now working!

--- js/boot.js
+++ js/boot.js
@@ -13,18 +13,29 @@ requirejs.config({
     }
 });
 
-require(['zui','pages/index/index'], function(zui) {
+require(['zui','pages/index/index', 'backbone'], function(zui, index, Backbone) {
     zui.logger.log('Zui Loaded!', { tags:'zui-all', eventName: 'zui-load' })
 
-    console.log(zui.factory['trigger'].fabricate());
+   // console.log(zui.factory['trigger'].fabricate());
 
-    var t1 = new zui.types.Trigger.fabricate();  
-    var t2 = new zui.types.Trigger.fabricate();  
+    var t1 =  zui.types.Trigger.fab({"id":"1111"});
+    t1.listenTo(t1, 'all', function(input){ console.log(arguments, input);});  
+    var t2 =  zui.types.Trigger.fab();  
+    
+    var t3 = new zui.types.Trigger();  
+    var t4 = new zui.types.Trigger({"id":"0000"});  
 
-    t1.setProp(156);
-    t2.setProp(333);
+     t1.setProp(156);
+     t2.setProp(333);
+     t3.setProp(999);
+     t4.setProp(888);
 
-    console.log(t1.getProp(), t2.getProp());
+    console.log(t1.get('id'), t2.get('id'));
+    console.log(t3.getProp(156), t4.getProp(156));
+    console.log(t1 instanceof zui.types.Trigger);
+    console.log(t3 , t4);
+
+    t1.trigger('tEvent', { level : 0 });
 
     //require(['page/home']);
     // require(['zui/zui_init'], function(zui) {
--- js/zui/models/TriggerModel.js
+++ js/zui/models/TriggerModel.js
@@ -1,136 +1,142 @@
-define(['backbone', 
+define(['underscore', 'backbone', 
     'zuiRoot/common',
-    'zuiRoot/logger'], function(Backbone, Common, Logger){
-        var privateScope = (function() {
-            var _ruleLinkages = {};
-            var testProp = 0;
-
-            return { 
-                defaults : {
-                    'id' : Common.genId(),
-                    'keepAlive': false,
-                    'resetAfterFire': false,
-                    'resetAfterEval': false,
-                    'lastPrimed': 0,
-                    'firedCount': 0,
-                    'state' : 'init'
-                },
-                
-                actions : [], // array of function pointers
+    'zuiRoot/logger'], function(_, Backbone, Common, Logger){
+        var _initial;
+        var generateScope = function(settings){
+            return new (function(settings){
+                settings = typeof settings === 'undefined' ? {} : settings;
+                var _ruleLinkages = {};
+                var testProp = 0;
     
-                initialize : function(){   
-                    Logger.log('Trigger ' + this.get('id') + ' Created', { filter: 'create' });
-                    this.prime();
-                },
-    
-                // CONSTRUCTOR 
-                state: function() {	return this.get('state') },
-                
-                setProp: function(value) { testProp = value; },
-                getProp: function() { return testProp; },
-                
-                addLinkage: function(link){
-                    // add new linkages  
-                    //check link type,
-                    _ruleLinkages.add(link);
-                },
-                
-                // AKA initialize
-                prime: function(){
-                    // if event based, we need trigger target or global space filter
-                        // listen to subs
-                        // payload match
-                    // if time based
-                        // timeA (at a point in the future): setListenInterval
-                        // timeB 
-                    // if event and time based, do both
-    
-                    // if isSet loop through primeConditions and evaluate 
-    
-                    //lastPrimed : 0, // date.now
-                    // isSet = true
-                    // return isSet;
-    
-                    // if(_state !== 'unprimed') {
-                    //     return;
-                    // }
+                return { 
+                    defaults : {
+                        'id' : settings.id ? settings.id : Common.genId(),
+                        'state' : 'unprimed',
+                        'keepAlive': false,
+                        'resetAfterFire': false,
+                        'resetAfterEval': false,
+                        'lastPrimed': 0,
+                        'firedCount': 0,
+                        'state' : 'init'
+                        //'echo'
+                    },
+                    actions : [], // array of function pointers
+        
+                    initialize : function(){   
+                        Logger.log('Trigger ' + this.get('id') + ' Created', { filter: 'create' });
+                        this.prime();
+                    },
+        
+                    // CONSTRUCTOR 
+                    state: function() {	return this.get('state') },
                     
-                    // loop over rules and route subs to eval, check for listenTo
-                    // for(var key in _ruleLinkages) {
-                    //     var settings = {};
+                    setProp: function(value) { testProp = value; },
+                    getProp: function() { return testProp; },
+                    
+                    addLinkage: function(link){
+                        // add new linkages  
+                        //check link type,
+                        _ruleLinkages.add(link);
+                    },
+                    
+                    // AKA initialize
+                    prime: function(){
+                        // if event based, we need trigger target or global space filter
+                            // listen to subs
+                            // payload match
+                        // if time based
+                            // timeA (at a point in the future): setListenInterval
+                            // timeB 
+                        // if event and time based, do both
+        
+                        // if isSet loop through primeConditions and evaluate 
+        
+                        //lastPrimed : 0, // date.now
+                        // isSet = true
+                        // return isSet;
+        
+                        // if(_state !== 'unprimed') {
+                        //     return;
+                        // }
                         
-                    //     settings.trigger = _trigger;
-                    //     settings.vars = _vars;
+                        // loop over rules and route subs to eval, check for listenTo
+                        // for(var key in _ruleLinkages) {
+                        //     var settings = {};
+                            
+                        //     settings.trigger = _trigger;
+                        //     settings.vars = _vars;
+                            
+                        //     _ruleLinkages[key].prime(settings); 
+                        // }
+                    },
+                    
+                    fire: function() {
+                        /* MODIFIERS POST-FIRE / EVAL / ACTION
+                            keepAlive
+                            resetAfterFire
+                            stickyRules
+                            count / increment(x)
+                            average, sum
+                            max
+                            min
+                        */
+                        // if(_action && typeof _action === 'function'){
+                        //     _action();
+                        // }
+                        // else {
+                        //     console.log('could not fire action. invalid type');
+                        // }
                         
-                    //     _ruleLinkages[key].prime(settings); 
-                    // }
-                },
-                
-                fire: function() {
-                    /* MODIFIERS POST-FIRE / EVAL / ACTION
-                        keepAlive
-                        resetAfterFire
-                        stickyRules
-                        count / increment(x)
-                        average, sum
-                        max
-                        min
-                    */
-                    // if(_action && typeof _action === 'function'){
-                    //     _action();
-                    // }
-                    // else {
-                    //     console.log('could not fire action. invalid type');
-                    // }
+                        // if(!_keepAlive) {
+                        //     _this.cleanup();
+                        // } else if(_resetAfterFire) {
+                        //     _this.reset();
+                        // }
+                    },
                     
-                    // if(!_keepAlive) {
-                    //     _this.cleanup();
-                    // } else if(_resetAfterFire) {
-                    //     _this.reset();
-                    // }
-                },
-                
-                reset: function() {
-                    _vars = {};
-                    for(var key in _ruleLinkages) {
-                        if(_ruleLinkages[key].resetWithTrigger){	
-                            _ruleLinkages[key].reset(); 
+                    reset: function() {
+                        _vars = {};
+                        for(var key in _ruleLinkages) {
+                            if(_ruleLinkages[key].resetWithTrigger){	
+                                _ruleLinkages[key].reset(); 
+                            }
                         }
-                    }
-                },
-                
-                cleanup: function() {
-                    // do some cleanup actions,
-                    // unsub any listeners
-                    // call parent to delete trigger?
+                    },
                     
-                    for(var key in _ruleLinkages) {
-                        if(_ruleLinkages[key].resetWithTrigger){	
-                            _ruleLinkages[key].cleanup(); 
+                    cleanup: function() {
+                        // do some cleanup actions,
+                        // unsub any listeners
+                        // call parent to delete trigger?
+                        
+                        for(var key in _ruleLinkages) {
+                            if(_ruleLinkages[key].resetWithTrigger){	
+                                _ruleLinkages[key].cleanup(); 
+                            }
                         }
+                    },
+                    
+                    evaluate: function(fireAfterEvaluate) {
+                        return _evaluateLinkages(fireAfterEvaluate);
                     }
-                },
-                
-                evaluate: function(fireAfterEvaluate) {
-                    return _evaluateLinkages(fireAfterEvaluate);
-                }
-            };
-        })();
-
+                };
+            })(settings);
+        };
+        
         //console.log(Backbone, Common, Logger);
         var staticMethods = (function() {
             
             return {
-                fabricate: function( settings ){
-                    console.log(this);
-                    settings = typeof settings === 'undefined' ? {} : settings;
-                    var trigger = new (Types.Trigger.extend(privateScope))();
+                fab: function( settings ){
+                    var trigger = new (_initial.extend(generateScope(settings)))();
                     return trigger;
-                }
+                },
+                fabFromJson: function(json) {
+                    return JSON.parse(json);
+                },
             }
         })();
 
         //console.log(staticMethods);
-
-        return Backbone.Model.extend(privateScope, staticMethods);
+        _initial = Backbone.Model.extend(generateScope(), staticMethods);
+        return _initial
 });
